af509bf51c53072b8c0980907a34226a866eaa99
FlorValdez <fvaldezsoriano01@gmail.com>
2024-08-15 09:39:07 -0400

    Delete ml.py

--- ml.py
+++
@@ -1,9 +0,0 @@
-#Funciones ML
-
-
-# Separar características (X) y variable objetivo (y) del conjunto de datos de entrenamiento y prueba
-X_train = train_data[['humidity', 'wind_speed', 'meanpressure']]
-y_train = train_data['meantemp']
-
-X_test = test_data[['humidity', 'wind_speed', 'meanpressure']]
-y_test = test_data['meantemp']
